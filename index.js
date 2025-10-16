7c311fb4b2e90d05d45b03371b888be5f3dea957
Umran <mohammedumran.0416@gmail.com>
2025-10-16 21:10:57 +0100

    made it so that i am able to toggle read on and off and remove entries

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container')
 
-const myLibrary = []
+let myLibrary = []
 
 class Book {
     constructor(title, author, numberOfPages, read) {
@@ -24,18 +24,11 @@ function addBookToLibrary(title,author,numberOfPages,read){
   
 addBookToLibrary('AOT', 'Isayama', 139, true)
 addBookToLibrary('OP', 'Oda', 1150, true)
-addBookToLibrary('JJK', 'Kishimoto', 250 , true)
-addBookToLibrary('AOT', 'Isayama', 139, true)
-addBookToLibrary('OP', 'Oda', 1150, true)
-addBookToLibrary('JJK', 'Kishimoto', 250 , true)
-addBookToLibrary('AOT', 'Isayama', 139, true)
-addBookToLibrary('OP', 'Oda', 1150, true)
-addBookToLibrary('JJK', 'Kishimoto', 250 , true)
-addBookToLibrary('AOT', 'Isayama', 139, true)
-addBookToLibrary('OP', 'Oda', 1150, true)
-addBookToLibrary('JJK', 'Kishimoto', 250 , true)
+addBookToLibrary('JJK', 'Kishimoto', 250 , false)
+
 
 function displayBooks(){
+    container.innerHTML =''
 
     for(let i = 0; i < myLibrary.length; i++){
 
@@ -43,19 +36,32 @@ function displayBooks(){
         const card = document.createElement('div')
         card.classList.add('book-card')
 
-        card.innerHTML = 
-        `
-            <h3>Name: ${book.title}</h3>
-            <p>Author: ${book.author}</p>
-            <p>Pg No: ${book.numberOfPages}</p>
-            <button>Remove</button>
-            <button>${book.read ? 'Read': 'Not Read'}</button>
+        card.innerHTML = `
+            <h3 class="book-info" >Name: ${book.title}</h3>
+            <p class="book-info">Author: ${book.author}</p>
+            <p class="book-info">Pg No: ${book.numberOfPages}</p>
+            <button class="remove">Remove</button>
+            <button class="toggleBtn" id=${book.read ? 'read': 'not-read'}>${book.read ? 'Read': 'Not Read'}</button>`
 
-        `
+            const toggleBtn = card.querySelector('.toggleBtn')
+            toggleBtn.addEventListener('click', () => {
+                book.toggleRead();
+                displayBooks();
+                
+            })
+
+            const removeBtn = card.querySelector('.remove')
+            removeBtn.addEventListener('click', () =>{
+                myLibrary = myLibrary.filter(b => b.id !== book.id)
+                displayBooks();
+
+            })            
+
      container.appendChild(card)
     }
 
     
 }
+
 
 displayBooks();
